da1624ef032c3d49cd3937b08bbef141e63193a0
Ricardo Bezerra <niverton.neves@gmail.com>
2024-05-05 21:46:49 -0300

    transition between mobile and web navbar

--- src/components/HeaderComponent/HeaderComponent.vue
+++ src/components/HeaderComponent/HeaderComponent.vue
@@ -1,5 +1,56 @@
 <template>
-  <div class="container">
+  <div class="mobile-version" v-if="this.$vuetify.display.xs">
+    <v-card :style="{ backgroundColor: presentTheme }">
+      <v-btn variant="text" @click="toggleNavDrawer">
+        <svg-icon type="mdi" :path="menu"></svg-icon>
+      </v-btn>
+      <div class="logo"><span>world</span><span>news</span></div>
+      <v-layout>
+        <v-navigation-drawer
+          :mobile="true"
+          v-model="drawer"
+          :style="{ backgroundColor: presentTheme }"
+        >
+          <div class="header-of-actions">
+            <div class="logo nav-drawer-bar">
+              <span class="logo-world">world</span><span class="logo-news">news</span>
+            </div>
+            <div class="close-btn">
+              <v-btn variant="text" @click="closeDrawer">
+                <svg-icon type="mdi" :path="close" class="close-icon"></svg-icon>
+              </v-btn>
+            </div>
+          </div>
+          <v-list :items="items"></v-list>
+        </v-navigation-drawer>
+      </v-layout>
+
+      <div class="flags flags-mobile">
+        <img
+          v-if="this.$i18n.locale !== 'pt'"
+          src="@/assets/images/flags/portugal.png"
+          alt="portugal"
+          width="35"
+          @click="handleChangeLocale('pt')"
+        />
+        <img
+          v-if="this.$i18n.locale !== 'en'"
+          src="@/assets/images/flags/usa.png"
+          alt="usa"
+          width="35"
+          @click="handleChangeLocale('en')"
+        />
+        <img
+          v-if="this.$i18n.locale !== 'br'"
+          src="@/assets/images/flags/brazil.png"
+          alt="brazil"
+          width="35"
+          @click="handleChangeLocale('br')"
+        />
+      </div>
+    </v-card>
+  </div>
+  <div class="web-version" v-if="!this.$vuetify.display.xs">
     <div class="nav-container">
       <div class="logo" :style="{ backgroundColor: presentTheme }">
         <span class="logo-world">world</span><span class="logo-news">news</span>
@@ -40,16 +91,18 @@
         />
       </div>
     </div>
-    <div class="second">
+    <div class="second-nav" :style="{ backgroundColor: presentTheme }">
       <nav class="nav-subcontainer">
         <div class="nav-itens">
-          <span> Home </span>
-          <span> Business </span>
-          <span> Entertainment </span>
-          <span> Health </span>
-          <span> Science </span>
-          <span> Sports </span>
-          <span> Technology </span>
+          <span
+            v-for="item in items"
+            :key="item"
+            @mouseover="hoverItem = item.value"
+            @mouseleave="hoverItem = null"
+            @click="filterByArticle(item.value)"
+            :class="{ hovered: hoverItem === item.value }"
+            >{{ item.title }}</span
+          >
         </div>
       </nav>
     </div>
@@ -59,7 +112,7 @@
 <script>
 import SvgIcon from '@jamescoyle/vue-icon'
 import { setI18nLanguage } from '@/plugins/i18n.js'
-import { mdiMagnify, mdiAccount } from '@mdi/js'
+import { mdiMagnify, mdiAccount, mdiMenu, mdiClose } from '@mdi/js'
 import themes from '@/styles/scss/temp.js'
 
 export default {
@@ -71,10 +124,36 @@ export default {
     return {
       magnify: mdiMagnify,
       account: mdiAccount,
+      menu: mdiMenu,
+      close: mdiClose,
+      drawer: false,
+      items: [],
+      selectedArticle: 'all',
+      hoverItem: null,
       presentTheme: ''
     }
   },
   methods: {
+    updateItems(locale) {
+      this.items = [
+        { title: this.$t('header.navItens.home', locale), value: 'home' },
+        { title: this.$t('header.navItens.business', locale), value: 'business' },
+        { title: this.$t('header.navItens.entertainment', locale), value: 'entertainment' },
+        { title: this.$t('header.navItens.health', locale), value: 'health' },
+        { title: this.$t('header.navItens.science', locale), value: 'science' },
+        { title: this.$t('header.navItens.sports', locale), value: 'sports' },
+        { title: this.$t('header.navItens.technology', locale), value: 'technology' }
+      ]
+    },
+    closeDrawer() {
+      this.drawer = false
+    },
+    toggleNavDrawer() {
+      this.drawer = !this.drawer
+    },
+    filterByArticle(article) {
+      this.selectedArticle = article
+    },
     changeTheme(theme) {
       const themeMap = {
         pt: themes.pt.theme,
@@ -89,9 +168,16 @@ export default {
       this.changeTheme(locale)
     }
   },
+  watch: {
+    '$i18n.locale'(newLocale) {
+      this.updateItems(newLocale)
+    }
+  },
   mounted() {
-    setI18nLanguage(localStorage.getItem('locale'))
-    this.changeTheme(localStorage.getItem('locale'))
+    const locale = localStorage.getItem('locale')
+    setI18nLanguage(locale)
+    this.changeTheme(locale)
+    this.updateItems(locale)
   }
 }
 </script>
