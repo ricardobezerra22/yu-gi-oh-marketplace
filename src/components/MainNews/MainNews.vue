d7611bb409d297bf9062befd776c0b1d1c0aa31b
Ricardo Bezerra <niverton.neves@gmail.com>
2024-05-09 14:04:19 -0300

    Filter valid articles

--- src/components/MainNews/MainNews.vue
+++ src/components/MainNews/MainNews.vue
@@ -6,14 +6,14 @@
     <div class="news-teaser">
       <div v-if="mainArticle" class="news-teaser-main">
         <img
-          :src="mainArticle.urlToImage"
+          :src="mainArticle.urlToImage ? mainArticle.urlToImage : defaultImage"
           alt="Main Article"
           height="auto"
           width="400px"
-          class="news-teaser-img extraneous"
+          class="news-teaser-img extraneous main-article-img"
         />
-        <h2 class="news-teaser-title">{{ mainArticle.title }}</h2>
-        <p class="news-teaser-description">{{ mainArticle.description }}</p>
+        <h2 class="news-teaser-title">{{ mainArticle.category }}</h2>
+        <p class="news-teaser-description">{{ mainArticle.title }}</p>
         <a :href="mainArticle.url" target="_blank" class="news-teaser-link">
           <span>{{ $t('newsTeaser.fullArticle') }} <span class="mdi mdi-open-in-new"></span></span>
         </a>
@@ -22,14 +22,14 @@
       <div class="news-teaser-extraneous">
         <div v-for="(article, index) in additionalArticles" :key="index">
           <img
-            :src="article.urlToImage"
+            :src="article.urlToImage ? article.urlToImage : defaultImage"
             alt="Additional Article"
             height="100px"
             class="news-teaser-img extraneous"
           />
-          <h2 class="news-teaser-title">{{ article.title }}</h2>
+          <h2 class="news-teaser-title">{{ article.category }}</h2>
           <p class="news-teaser-description extraneous-desc">
-            {{ truncatedDescription(article.description) }}
+            {{ article.title }}
           </p>
           <a :href="article.url" target="_blank" class="news-teaser-link">
             <span class="news-teaser-link-article"
@@ -51,6 +51,7 @@ export default {
   data() {
     return {
       articles: [],
+
       categories: [
         'business',
         'entertainment',
@@ -60,17 +61,20 @@ export default {
         'sports',
         'technology'
       ],
+      defaultImage:
+        'https://t3.ftcdn.net/jpg/03/27/55/60/360_F_327556002_99c7QmZmwocLwF7ywQ68ChZaBry1DbtD.jpg',
       mainArticle: null,
       additionalArticles: []
     }
   },
   methods: {
-    truncatedDescription(value, maxLength = 88) {
-      if (value.length > maxLength) {
-        return value.substring(0, maxLength) + '...'
-      } else return value
-    },
     async fetchTopHeadlines() {
+      const verifyUrl = (data, findData) => {
+        if (!data) {
+          data = findData.find((article) => article?.title)
+        }
+      }
+
       try {
         const locale = localStorage.getItem('locale')
 
@@ -81,13 +85,17 @@ export default {
         const mainCategory = this.categories[0]
         const mainResponse = await getTopHeadlines(locale, mainCategory)
         const mainArticlesData = mainResponse.data.articles
-        this.shuffleArray(mainArticlesData)
-        const mainArticleData = mainArticlesData[0]
+
+        // Find the first article with valid title
+        let validMainArticleData = mainArticlesData.find((article) => article.title !== '[Removed]')
+
+        verifyUrl(validMainArticleData, mainArticlesData)
+
         this.mainArticle = {
-          title: mainCategory.toUpperCase(),
-          description: mainArticleData.description,
-          url: mainArticleData.url,
-          urlToImage: mainArticleData.urlToImage
+          category: mainCategory.toUpperCase(),
+          title: validMainArticleData.title,
+          url: validMainArticleData.url,
+          urlToImage: validMainArticleData.urlToImage
         }
 
         // Fetch additional articles
@@ -96,19 +104,21 @@ export default {
           additionalCategories.map(async (category) => {
             const response = await getTopHeadlines(locale, category)
             const articlesData = response.data.articles
-            this.shuffleArray(articlesData)
-            const articleData = articlesData[0]
+            let validArticleData = articlesData.find((article) => article.title !== '[Removed]')
+
+            verifyUrl(validArticleData, articlesData)
+
             return {
-              title: category.toUpperCase(),
-              description: articleData.description,
-              url: articleData.url,
-              urlToImage: articleData.urlToImage
+              category: category.toUpperCase(),
+              title: validArticleData.title,
+              url: validArticleData.url,
+              urlToImage: validArticleData.urlToImage
             }
           })
         )
 
         // Filter out main article from additional articles
-        const mainArticleUrl = mainArticleData.url
+        const mainArticleUrl = validMainArticleData.url
         this.additionalArticles = additionalResponses.filter(
           (article) => article.url !== mainArticleUrl
         )
@@ -120,10 +130,10 @@ export default {
     shuffleArray(array) {
       // Fisher-Yates shuffle algorithm
       for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1))
+        const randomized = Math.floor(Math.random() * (i + 1))
         const temp = array[i]
-        array[i] = array[j]
-        array[j] = temp
+        array[i] = array[randomized]
+        array[randomized] = temp
       }
     }
   },
