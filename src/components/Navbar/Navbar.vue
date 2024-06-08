3e00c7fd8cf095550d3ad24a2c7327712388862f
Ricardo Bezerra <niverton.neves@gmail.com>
2024-06-08 15:42:08 -0300

    feat: responsive navbar

---
+++ src/components/Navbar/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div></div>
+</template>
+
+<script setup>
+import { watch, ref, onMounted } from 'vue'
+import { useDisplay } from 'vuetify'
+
+const { sm } = useDisplay()
+const menuItems = ref([
+  { title: 'Home', value: 'home' },
+  { title: 'Todas As Cartas', value: 'all-cards' },
+  { title: 'Trocas em aberto', value: 'open-trades' },
+  { title: 'Inventário', value: 'inventory' },
+  { title: 'Login', value: 'login' },
+  { title: 'Sair', value: 'logout' }
+])
+const isMobile = ref(false)
+
+watch(sm, () => {
+  isMobile.value = sm.value
+})
+onMounted(() => {
+  isMobile.value = sm.value
+})
+</script>
+
+<style scoped lang="scss" src="./style.scss"></style>
--- src/views/HomeView/HomeView.vue
+++ src/views/HomeView/HomeView.vue
@@ -1,8 +1,58 @@
 <template>
   <div class="container-wrapper">
-    <p>Opa</p>
+    <div class="navbar" v-if="!isMobile">
+      <div class="logo-img">
+        <img src="@/assets/images/logo.png" alt="logo" width="128px" />
+      </div>
+      <div class="menu-items">
+        <v-menu v-model="menu" :close-on-content-click="false">
+          <template v-slot:activator="{ props }">
+            <v-icon v-bind="props" class="menu-items-icon">mdi-menu</v-icon>
+          </template>
+          <v-card min-width="300" theme="dark">
+            <v-list>
+              <v-list-item
+                prepend-icon="mdi mdi-account"
+                title="Example"
+                subtitle="example@gmail.com"
+              >
+              </v-list-item>
+            </v-list>
+
+            <v-divider></v-divider>
+
+            <v-list>
+              <v-list-item v-for="(item, i) in menuItems" :key="i" class="list-item-title">
+                {{ item.title }}
+              </v-list-item>
+            </v-list>
+          </v-card>
+        </v-menu>
+      </div>
+    </div>
+    <div class="navbar md" v-else>
+      <div class="logo-img">
+        <img src="@/assets/images/logo.png" alt="logo" width="128px" />
+      </div>
+      <div class="menu-items" v-for="(item, i) in menuItems" :key="i">
+        <span class="list-item-title"> {{ item.title }} </span>
+      </div>
+    </div>
   </div>
 </template>
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+
+const menu = ref(false)
+const isMobile = ref(true)
+const menuItems = ref([
+  { title: 'Home', value: 'home' },
+  { title: 'Todas As Cartas', value: 'all-cards' },
+  { title: 'Trocas em aberto', value: 'open-trades' },
+  { title: 'Inventário', value: 'inventory' },
+  { title: 'Login', value: 'login' },
+  { title: 'Sair', value: 'logout' }
+])
+</script>
 
 <style scoped lang="scss" src="./style.scss"></style>
