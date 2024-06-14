234aa5f1c07c049321c5005c8e025a69a6b212c2
Ricardo Bezerra <niverton.neves@gmail.com>
2024-06-14 02:54:16 -0300

    feat: home view

--- src/views/HomeView/HomeView.vue
+++ src/views/HomeView/HomeView.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="container-wrapper">
-    <p>Home</p>
+    <div class="home-view">
+      <div class="home-view-duel">
+        <img src="@/assets/images/yugi-duel.png" alt="duel" />
+      </div>
+      <div class="home-view-greetings">
+        <h1>Bem-vindo ao Yu-Gi-Oh!</h1>
+        <p>
+          Aqui você pode explorar todas as cartas do jogo Yu-Gi-Oh!, ver trocas em andamento, e
+          participar da comunidade para fazer suas próprias trocas e colecionar suas próprias
+          cartas.
+        </p>
+        <button class="cta-button">Comece a Explorar</button>
+      </div>
+    </div>
   </div>
 </template>
 <script setup></script>
